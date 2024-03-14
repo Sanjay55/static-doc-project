cc32ab42f5d4ba4510f6f43813f166cdffc918b4
vgsanjay <sanjay.varma@toothsi.in>
2024-03-14 19:06:52 +0530

    nextra integrate

--- next.config.js
+++ next.config.js
@@ -1,11 +1,18 @@
-module.exports = {
-  output: "export",
-  webpack: (cfg) => {
-    cfg.module.rules.push({
-      test: /\.md$/,
-      loader: "frontmatter-markdown-loader",
-      options: { mode: ["react-component"] },
-    });
-    return cfg;
-  },
-};
+// module.exports = {
+//   output: "export",
+//   webpack: (cfg) => {
+//     cfg.module.rules.push({
+//       test: /\.md$/,
+//       loader: "frontmatter-markdown-loader",
+//       options: { mode: ["react-component"] },
+//     });
+//     return cfg;
+//   },
+// };
+
+const withNextra = require("nextra")({
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.jsx",
+});
+
+module.exports = withNextra();
